d1bccb1e9e2e306b9724b19b67f57ed806365652
CESGomes <cesgomes@gmail.com>
2024-09-11 00:01:38 +0200

    Dummy Data para criação de posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
+posts = [
+    {"author": "CESGomes",
+     "Title": "Blog Post 1",
+     "Content": "First Post Content",
+     "DatePost": "09-Set-2024"},
+    {"author": "John Doe",
+     "Title": "Blog Post 2",
+     "Content": "Second Post Content",
+     "DatePost": "10-Set-2024"}
+    ]
+
 #Primeiro: Criar a função que irá ter a página home
 def home(request):
-    return render (request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render (request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
