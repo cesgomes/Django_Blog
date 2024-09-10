76aba671bf884a8241d73318adc24c419d40cec1
CESGomes <cesgomes@gmail.com>
2024-09-11 00:08:28 +0200

    Definição do título da página (passagem de parâmetros)

--- blog/views.py
+++ blog/views.py
@@ -9,14 +9,15 @@ posts = [
      "Title": "Blog Post 2",
      "Content": "Second Post Content",
      "DatePost": "10-Set-2024"}
-    ]
+]
 
-#Primeiro: Criar a função que irá ter a página home
+# Primeiro: Criar a função que irá ter a página home
 def home(request):
     context = {
         'posts': posts
     }
-    return render (request, 'blog/home.html', context)
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
