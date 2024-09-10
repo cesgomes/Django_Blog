90cedd20e3d2bba29a6bc3b1b301478e894be7ae
CESGomes <cesgomes@gmail.com>
2024-09-10 23:41:11 +0200

    Alteração da About Page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 #Primeiro: Criar a função que irá ter a página home
 def home(request):
     return render (request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse('<H2>About Page</h2')
+    return render(request, 'blog/about.html')
