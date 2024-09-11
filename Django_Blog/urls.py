620a68643e5a35aae3a01dfca7ef4a7c5770d24d
CESGomes <cesgomes@gmail.com>
2024-09-11 22:23:39 +0200

    Criação da página de registro

--- Django_Blog/urls.py
+++ Django_Blog/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-#Terceiro: Criar o caminho inicial
-    path('', include('blog.urls'))
+    path('register/', user_views.register, name='register'),
+    # Terceiro: Criar o caminho inicial
+    path('', include('blog.urls')),
 ]
