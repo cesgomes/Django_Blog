889dfcf2635c7f4117d7c51edccce3eac21a0e56
CESGomes <cesgomes@gmail.com>
2024-09-12 23:12:37 +0200

    Definição dos locais das medias

--- Django_Blog/settings.py
+++ Django_Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+# Diretorio onde serão gravados os arquivos
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'  # Local onde as medias serão acessadas pelo browser
