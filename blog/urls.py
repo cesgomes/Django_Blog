ca32e168df00b37d306a923aecde05b4692d50fd
CESGomes <cesgomes@gmail.com>
2024-09-13 23:10:46 +0200

    troca da função Home para a classe ListView

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 # Passo 2: Criar este arquivo e importar a URLPattern
 from django.urls import path
+from .views import PostListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 # Primeiro: Criar a função que irá ter a página home
@@ -8,6 +9,8 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
