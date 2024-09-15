3b0cd92833d575cca743a065d1dcdf722336fe64
Carlos Eduardo Gomes <cesgomes@gmail.com>
2024-09-16 00:39:47 +0100

    Criação dos templates, páginas e rotas para reset password

--- Django_Blog/urls.py
+++ Django_Blog/urls.py
@@ -27,9 +27,18 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
     # Terceiro: Criar o caminho inicial
     path('', include('blog.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
